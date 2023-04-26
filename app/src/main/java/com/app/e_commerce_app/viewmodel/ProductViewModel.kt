0665a114aa0d5dda61cfbc2f769efdf819cc9ebc
LAPTOP-182PLMJF\PC <20110615@student.hcmute.edu.vn>
2023-04-27 00:28:54 +0700

    display product by category id

--- app/src/main/java/com/app/e_commerce_app/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/app/e_commerce_app/viewmodel/ProductViewModel.kt
@@ -24,6 +24,16 @@ class ProductViewModel(application: Application) : ViewModel() {
         }
     }
 
+    fun getProductsByCategory(id : Int) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+
+        when (val response = productRepository.getProductsByCategory(id)) {
+            is NetWorkResult.Success -> emit(Resource.success(response.data.data))
+            is NetWorkResult.Error -> emit(Resource.error(null, response.message))
+            is NetWorkResult.Exception -> emit(Resource.error(null, response.e.message))
+        }
+    }
+
     class ProductViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
