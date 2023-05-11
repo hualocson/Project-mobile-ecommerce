e88907a9cff4b582add46bc0efa345161a7339a9
LAPTOP-182PLMJF\PC <20110615@student.hcmute.edu.vn>
2023-05-12 01:01:44 +0700

    hot fix upload image done

--- app/src/main/java/com/app/e_commerce_app/ui/UploadImageFragment.kt
+++ app/src/main/java/com/app/e_commerce_app/ui/UploadImageFragment.kt
@@ -1,14 +1,21 @@
 package com.app.e_commerce_app.ui
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.provider.MediaStore.Images
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -18,6 +25,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.net.toFile
 import androidx.fragment.app.viewModels
+import androidx.loader.content.CursorLoader
 import com.app.e_commerce_app.base.BaseFragment
 import com.app.e_commerce_app.databinding.FragmentProfileBinding
 import com.app.e_commerce_app.databinding.FragmentUploadImgBinding
@@ -101,7 +109,7 @@ class UploadImageFragment : BaseFragment<FragmentUploadImgBinding>(true) {
     private fun opqenImageChooser() {
         Intent(Intent.ACTION_PICK).also {
             it.type = "image/*"
-            val minTypes = arrayOf("image/jpeg","image/jpg","image/png")
+            val minTypes = arrayOf("image/jpg")
             it.putExtra(Intent.EXTRA_MIME_TYPES, minTypes)
             startActivityForResult(it, REQUEST_CODE_IMG)
         }
@@ -109,11 +117,31 @@ class UploadImageFragment : BaseFragment<FragmentUploadImgBinding>(true) {
 
     private fun uploadUserImage(){
         val uri = Uri.parse(mUri.toString())
-        val path = uri.path
-        val file: File = File(path)
-        val requestBody = file.path.toRequestBody("multipart/form-data".toMediaTypeOrNull())
-        val multipartBody = MultipartBody.Part.createFormData("avatar", file.name, requestBody)
-        userViewModel.uploadImage(multipartBody)
+        val path = RealPathUtil.getRealPath(requireContext(), uri)
+        val file = createFileFromRealPath(path)
+        val requestBody = createRequestBody(file!!)
+        val multipartBodyPart = createMultipartBodyPart(file!!, "avatar")
+        userViewModel.uploadImage(multipartBodyPart)
+    }
+
+    fun createFileFromRealPath(realPath: String?): File? {
+        if (realPath == null) {
+            return null
+        }
+        return try {
+            File(realPath)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun createRequestBody(file: File): RequestBody {
+        return RequestBody.create(file.toURI().toURL().toString().toMediaTypeOrNull(), file)
+    }
+
+    fun createMultipartBodyPart(file: File, fieldName: String): MultipartBody.Part {
+        val requestBody = createRequestBody(file)
+        return MultipartBody.Part.createFormData(fieldName, file.name, requestBody)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -128,6 +156,171 @@ class UploadImageFragment : BaseFragment<FragmentUploadImgBinding>(true) {
                     Log.d("uri", selectImgUri.toString())
                 }
             }
+        }
+    }
+    object RealPathUtil {
+
+        fun getRealPath(context: Context, fileUri: Uri): String? {
+            // SDK >= 11 && SDK < 19
+            return if (Build.VERSION.SDK_INT < 19) {
+                getRealPathFromURIAPI11to18(context, fileUri)
+            } else {
+                getRealPathFromURIAPI19(context, fileUri)
+            }// SDK > 19 (Android 4.4) and up
+        }
+
+        @SuppressLint("NewApi")
+        fun getRealPathFromURIAPI11to18(context: Context, contentUri: Uri): String? {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            var result: String? = null
+
+            val cursorLoader = CursorLoader(context, contentUri, proj, null, null, null)
+            val cursor = cursorLoader.loadInBackground()
+
+            if (cursor != null) {
+                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                cursor.moveToFirst()
+                result = cursor.getString(columnIndex)
+                cursor.close()
+            }
+            return result
+        }
+
+        /**
+         * Get a file path from a Uri. This will get the the path for Storage Access
+         * Framework Documents, as well as the _data field for the MediaStore and
+         * other file-based ContentProviders.
+         *
+         * @param context The context.
+         * @param uri     The Uri to query.
+         * @author Niks
+         */
+        @SuppressLint("NewApi")
+        fun getRealPathFromURIAPI19(context: Context, uri: Uri): String? {
+
+            val isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+
+            // DocumentProvider
+            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+                // ExternalStorageProvider
+                if (isExternalStorageDocument(uri)) {
+                    val docId = DocumentsContract.getDocumentId(uri)
+                    val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                    val type = split[0]
+
+                    if ("primary".equals(type, ignoreCase = true)) {
+                        return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                    }
+                } else if (isDownloadsDocument(uri)) {
+                    var cursor: Cursor? = null
+                    try {
+                        cursor = context.contentResolver.query(uri, arrayOf(MediaStore.MediaColumns.DISPLAY_NAME), null, null, null)
+                        cursor!!.moveToNext()
+                        val fileName = cursor.getString(0)
+                        val path = Environment.getExternalStorageDirectory().toString() + "/Download/" + fileName
+                        if (!TextUtils.isEmpty(path)) {
+                            return path
+                        }
+                    } finally {
+                        cursor?.close()
+                    }
+                    val id = DocumentsContract.getDocumentId(uri)
+                    if (id.startsWith("raw:")) {
+                        return id.replaceFirst("raw:".toRegex(), "")
+                    }
+                    val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads"), java.lang.Long.valueOf(id))
+
+                    return getDataColumn(context, contentUri, null, null)
+                } else if (isMediaDocument(uri)) {
+                    val docId = DocumentsContract.getDocumentId(uri)
+                    val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                    val type = split[0]
+
+                    var contentUri: Uri? = null
+                    when (type) {
+                        "image" -> contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        "video" -> contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                        "audio" -> contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                    }
+
+                    val selection = "_id=?"
+                    val selectionArgs = arrayOf(split[1])
+
+                    return getDataColumn(context, contentUri, selection, selectionArgs)
+                }// MediaProvider
+                // DownloadsProvider
+            } else if ("content".equals(uri.scheme!!, ignoreCase = true)) {
+
+                // Return the remote address
+                return if (isGooglePhotosUri(uri)) uri.lastPathSegment else getDataColumn(context, uri, null, null)
+            } else if ("file".equals(uri.scheme!!, ignoreCase = true)) {
+                return uri.path
+            }// File
+            // MediaStore (and general)
+
+            return null
+        }
+
+        /**
+         * Get the value of the data column for this Uri. This is useful for
+         * MediaStore Uris, and other file-based ContentProviders.
+         *
+         * @param context       The context.
+         * @param uri           The Uri to query.
+         * @param selection     (Optional) Filter used in the query.
+         * @param selectionArgs (Optional) Selection arguments used in the query.
+         * @return The value of the _data column, which is typically a file path.
+         * @author Niks
+         */
+        private fun getDataColumn(context: Context, uri: Uri?, selection: String?,
+                                  selectionArgs: Array<String>?): String? {
+
+            var cursor: Cursor? = null
+            val column = "_data"
+            val projection = arrayOf(column)
+
+            try {
+                cursor = context.contentResolver.query(uri!!, projection, selection, selectionArgs, null)
+                if (cursor != null && cursor.moveToFirst()) {
+                    val index = cursor.getColumnIndexOrThrow(column)
+                    return cursor.getString(index)
+                }
+            } finally {
+                cursor?.close()
+            }
+            return null
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is ExternalStorageProvider.
+         */
+        private fun isExternalStorageDocument(uri: Uri): Boolean {
+            return "com.android.externalstorage.documents" == uri.authority
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is DownloadsProvider.
+         */
+        private fun isDownloadsDocument(uri: Uri): Boolean {
+            return "com.android.providers.downloads.documents" == uri.authority
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is MediaProvider.
+         */
+        private fun isMediaDocument(uri: Uri): Boolean {
+            return "com.android.providers.media.documents" == uri.authority
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is Google Photos.
+         */
+        private fun isGooglePhotosUri(uri: Uri): Boolean {
+            return "com.google.android.apps.photos.content" == uri.authority
         }
     }
 }
