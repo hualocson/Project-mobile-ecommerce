c00065df6fe63e5f5758e490251dec1a5773b6bd
LAPTOP-182PLMJF\PC <20110615@student.hcmute.edu.vn>
2023-05-17 01:07:40 +0700

    url

--- app/src/main/java/com/app/e_commerce_app/data/api/ConstantsURL.kt
+++ app/src/main/java/com/app/e_commerce_app/data/api/ConstantsURL.kt
@@ -2,9 +2,10 @@ package com.app.e_commerce_app.data.api
 
 object ConstantsURL {
     // Endpoints
-    const val BASE_URL = "http://10.0.2.2:5001/api/v1/" // For local api
+//    const val BASE_URL = "https://9cab-42-117-100-7.ngrok-free.app/api/v1/" // For local api
 //    const val BASE_URL = "https://server-rests-api-mobile.onrender.com/api/v1/"
 //    const val BASE_URL = "https://server-rests-api-mobile-dev.onrender.com/api/v1/" //dev
+    const val BASE_URL = "https://79d6-42-117-100-7.ngrok-free.app/api/v1/"
     const val LOGIN_URL = "users/signin"
     const val USER_ADDRESS = "users/addresses"
     const val CHANGE_PASSWORD = "users/password"
@@ -19,9 +20,10 @@ object ConstantsURL {
     const val PRODUCT_BY_ID = "categories/products/{productId}"
     const val USER_UPLOAD_IMG = "users/avatar"
     const val EDIT_PROFILE = "users"
-
     const val SHIPPING_URL = "shippings"
     const val ORDER_URL = "orders"
     const val NEW_URL = "news"
     const val ORDER_BY_ID = "orders/{id}"
+    const val ORDER_UPDATE = "orders/{id}/status"
+    const val PRODUCT_ITEMS = "categories/products/items/{productItemId}"
 }
